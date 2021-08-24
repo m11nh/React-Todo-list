0f43b9185693884191a5146178132f30e06ab83f
Minh Nguyen <minh.nguyen19997@hotmail.com>
2021-08-25 09:04:18 +1000

    basic functionality of adding task complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import reactDom from 'react-dom';
+
+class InputForm extends React.Component {
+  render() {
+    return (
+      <form onSubmit={(event) => this.props.handleSubmit(event)}>
+        <label>
+          Name: 
+          <input type="text" value={this.props.value} onChange={(event) => this.props.handleChange(event)}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 function Task(props) {
   return (
@@ -10,31 +26,56 @@ function Task(props) {
   )
 }
 
-function App() {
-  let incompleteTasks = [] 
-  let completedTasks = []; 
-  return (
-    <div>
+class App extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      incompleteTasks : ["gee", "lol"], 
+      completedTasks : ["buddy"] ,
+      value: ''
+    }
+  }
+
+  handleSubmit(event) {
+    // alert('A name was submitted' + this.state.value);
+    let incompleteTasksNew = this.state.incompleteTasks; 
+    incompleteTasksNew.push(this.state.value)
+    this.setState({
+      incompleteTasks : incompleteTasksNew, 
+      completedTasks : this.state.completedTasks
+    })
+    event.preventDefault(); 
+  }
+
+  handleChange(event) {
+    this.setState({
+      incompleteTasks : this.state.incompleteTasks, 
+      completedTasks : this.state.completedTasks, 
+      value : event.target.value
+    })
+  }
+
+  render() {
+    return (
       <div>
-        <form>
-          <input type="text"/>
-          <button onclick={addTask}> Add Task </button>
-        </form>
+        <div>
+          <InputForm value={this.state.value} handleChange={(event) => this.handleChange(event)} handleSubmit={(event) => this.handleSubmit(event)}/>
+        </div>
+        <h1>Incomplete Tasks</h1>
+        <div>
+          {this.state.incompleteTasks.map((element) => {
+            return <Task name={element}/>
+          })}
+        </div>
+        <h1>Complete Tasks</h1>
+        <div>
+          {this.state.completedTasks.map((element) => {
+            return <Task name={element}/>
+          })}
+        </div>
       </div>
-      <h1>Incomplete Tasks</h1>
-      <div>
-        {incompleteTasks.map((element) => {
-          return <Task name={element}/>
-        })}
-      </div>
-      <h1>Complete Tasks</h1>
-      <div>
-        {completedTasks.map((element) => {
-          return <Task name={element}/>
-        })}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 function addTask() {
 
