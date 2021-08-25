2e01fe6bbaf16eb1fdf03023b116cf6124c0afb3
Minh Nguyen <minh.nguyen19997@hotmail.com>
2021-08-25 21:38:08 +1000

    completed basic functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ class InputForm extends React.Component {
     return (
       <form onSubmit={(event) => this.props.handleSubmit(event)}>
         <label>
-          Name: 
           <input type="text" value={this.props.value} onChange={(event) => this.props.handleChange(event)}/>
         </label>
         <input type="submit" value="Submit" />
@@ -17,40 +16,63 @@ class InputForm extends React.Component {
   }
 }
 
-function Task(props) {
-  return (
-    <div>
-      <span>{props.name}</span>
-      <input type="checkbox"/>
-    </div>
-  )
+class Task extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      checked:this.props.checked, 
+      name: this.props.name,
+      id: this.props.id
+    };
+  }
+
+  handleClick(e) {
+    return this.props.handleClick(e);
+  }
+
+  render() {
+    return (
+      <div>
+        <span>{this.state.name}</span>
+        <input type="checkbox" onClick={()=>this.handleClick(this.state.id)} checked={this.state.checked}/>
+      </div>
+    )
+  }
 }
 
 class App extends React.Component {
   constructor(props) {
     super(props); 
     this.state = {
-      incompleteTasks : ["gee", "lol"], 
-      completedTasks : ["buddy"] ,
-      value: ''
+      tasks: [],
+      value: '', 
     }
   }
 
   handleSubmit(event) {
     // alert('A name was submitted' + this.state.value);
-    let incompleteTasksNew = this.state.incompleteTasks; 
-    incompleteTasksNew.push(this.state.value)
+    let new_tasks = this.state.tasks; 
+    let len = this.state.tasks.length; 
+    new_tasks.push({id: len, value: this.state.value, checked: false});
     this.setState({
-      incompleteTasks : incompleteTasksNew, 
-      completedTasks : this.state.completedTasks
+      tasks: new_tasks,
+      value: ''
     })
     event.preventDefault(); 
   }
 
+  handleTaskClick(taskId) {
+    let new_tasks = this.state.tasks;
+    new_tasks[taskId].checked = true; 
+    this.setState({
+      tasks: new_tasks, 
+      value: this.state.value
+    })
+  }
+
   handleChange(event) {
     this.setState({
-      incompleteTasks : this.state.incompleteTasks, 
-      completedTasks : this.state.completedTasks, 
+      tasks: this.state.tasks,
       value : event.target.value
     })
   }
@@ -63,16 +85,18 @@ class App extends React.Component {
         </div>
         <h1>Incomplete Tasks</h1>
         <div>
-          {this.state.incompleteTasks.map((element) => {
-            return <Task name={element}/>
+          {this.state.tasks.map((e) => {
+            if (e.checked === false) {
+              return <Task id={e.id} name={e.value} checked={e.checked} handleClick={(event) => this.handleTaskClick(event)}/>
+            }
           })}
         </div>
-        <h1>Complete Tasks</h1>
+        {/* <h1>Complete Tasks</h1>
         <div>
           {this.state.completedTasks.map((element) => {
             return <Task name={element}/>
           })}
-        </div>
+        </div> */}
       </div>
     );
   }
